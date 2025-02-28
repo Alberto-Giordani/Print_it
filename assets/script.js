e2104752a893f046a0f32b0aa725fe7e9f8bcc85
Alberto Giordani <albertogiordani@ymail.com>
2025-02-28 10:19:43 +0100

    Défilement infini carrousel

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,72 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
+		"image":"./assets/images/slideshow/slide1.jpg",
 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
+		"image":"./assets/images/slideshow/slide2.jpg",
 		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
+		"image":"./assets/images/slideshow/slide3.jpg",
 		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
+		"image":"./assets/images/slideshow/slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
 
+const arrow_left = document.querySelector('.arrow_left');
+const arrow_right = document.querySelector('.arrow_right');
 
+const dots = document.querySelector('.dots');
+dots.innerHTML = "";
+
+for (let j = 0; j < slides.length; j++) {
+	const bulletPoint = document.createElement('i');
+	bulletPoint.classList.add('dot');
+	if(j === 0) {
+		bulletPoint.classList.add('dot_selected');
+	}
+	dots.appendChild(bulletPoint);
+}
+
+let baliseImage = document.querySelector('.banner-img');
+let textImage = document.querySelector('#banner p');
+const dot = document.querySelectorAll('.dot');
+let i = 0;
+
+arrow_right.addEventListener('click', () => {
+	console.log("click flèche droite");
+	selectedRemover(i);
+	if (i < (slides.length - 1)) {
+		i++;
+	}
+	else {
+		i = 0;
+	}
+	progressionBanner(i);
+});
+
+arrow_left.addEventListener('click', () => {
+	console.log("click flèche gauche");
+	selectedRemover(i);
+	if (i > 0) {
+		i--;
+	}
+	else {
+		i = (slides.length - 1);
+	}
+	progressionBanner(i);
+});
+
+function selectedRemover(i) {
+	dot[i].classList.remove('dot_selected');
+}
+
+function progressionBanner(i) {
+	baliseImage.src = slides[i].image;
+	textImage.innerHTML = slides[i].tagLine;
+	dot[i].classList.add('dot_selected');
+}
